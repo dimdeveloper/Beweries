845b4818b0b9dca905efeb0bb10a0f0ff05db71f
dimDev <dimel80@yahoo.com>
2023-12-20 00:46:29 +0200

    - added webLink

--- BreweriesList/BreweryTableCell.swift
+++ BreweriesList/BreweryTableCell.swift
@@ -15,7 +15,7 @@ class BreweryTableCell: UITableViewCell {
     
     @IBOutlet var name: [UILabel]!
     @IBOutlet weak var phone: UILabel!
-    @IBOutlet weak var website: UILabel!
+    @IBOutlet weak var webLink: UITextView!
     
     @IBOutlet weak var infoView: UIView!
     @IBOutlet weak var countryLabelView: UIView!
@@ -35,21 +35,29 @@ class BreweryTableCell: UITableViewCell {
             label.text = brewery.name
         }
         self.phone.text = brewery.phone
-        self.website.text = brewery.websiteUrl
+        
         self.country.text = brewery.country
         self.state.text = brewery.state
         self.city.text = brewery.city
         self.street.text = brewery.street
-        
         self.phoneLabelView.update(with: brewery.phone)
         self.webLabelView.update(with: brewery.websiteUrl)
         self.countryLabelView.update(with: brewery.country)
         self.stateLabelView.update(with: brewery.state)
         self.cityLabelView.update(with: brewery.city)
         self.streetLabelView.update(with: brewery.street)
+        
+        self.webLink.textContainerInset = .zero
+
+        guard let webLink = brewery.websiteUrl else {return}
+        let attributedString = NSMutableAttributedString(string: brewery.websiteUrl ?? "")
+        attributedString.addAttribute(.link, value: webLink, range: NSRange(webLink.startIndex..., in: webLink))
+            
+        self.webLink.attributedText = attributedString
     }
     
     private func setupView(){
+        
         headerView.layer.cornerRadius = 12
         infoView.layer.cornerRadius = 12
         headerView.layer.borderWidth = 1
@@ -67,4 +75,11 @@ extension UIView {
             self.isHidden = true
         }
     }
+}
+
+extension BreweryTableCell: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+                UIApplication.shared.open(URL)
+                return false
+            }
 }
